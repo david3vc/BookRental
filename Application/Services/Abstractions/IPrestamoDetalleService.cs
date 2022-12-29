d675ebaa0155deb0f014e63b18af599a1050d11c
david3vc <dveracabanillas@hotmail.com>
2022-12-29 09:35:20 -0500

    Prestamo detalle Service implemented

---
+++ Application/Services/Abstractions/IPrestamoDetalleService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Base;
+using Application.Dtos.PrestamoDetalles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Abstractions
+{
+    public interface IPrestamoDetalleService : IServiceBase<PrestamoDetalleDto, PrestamoDetalleFormDto, PrestamoDetalleIdDto>, IServicePaginated<PrestamoDetalleDto>
+    {
+    }
+}
